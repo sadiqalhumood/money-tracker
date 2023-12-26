ebc801770e4ff3646a263ddfe30758bed0342a77
Sadiq <113123466+sadiqalhumood@users.noreply.github.com>
2023-12-26 06:15:52 +0300

    html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>
+        <div className="basic">
+          <input type="text" placeholder="+200 new phone" />
+          <input type="datetime-local" />
+        </div>
+        <div className='desc'>
+          <input type="text" placeholder="Description" />
+        </div>
+        <button type="submit" className="btn">Add Transaction</button>
+      </form>
+      <div className='transactions'>
+        <div className="transaction">
+          <div className='left'>
+            <div className='name'>New Phone</div>
+            <div className='desc'>It was time for new phone</div>
+          </div>
+          <div className='right'>
+            <div className='price'>$200.00</div>
+            <div className='datetime'>12-02-2022 08:00</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className='left'>
+            <div className='name'>New Phone</div>
+            <div className='desc'>It was time for new phone</div>
+          </div>
+          <div className='right'>
+            <div className='price'>$200.00</div>
+            <div className='datetime'>12-02-2022 08:00</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className='left'>
+            <div className='name'>New Phone</div>
+            <div className='desc'>It was time for new phone</div>
+          </div>
+          <div className='right'>
+            <div className='price'>$200.00</div>
+            <div className='datetime'>12-02-2022 08:00</div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
