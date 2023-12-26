bec90e74adc8d1f8be59db85249a5365befabd94
Sadiq <113123466+sadiqalhumood@users.noreply.github.com>
2023-12-26 09:50:35 +0300

    html and css done, now we need to make things functional

--- src/App.js
+++ src/App.js
@@ -17,31 +17,31 @@ function App() {
       <div className='transactions'>
         <div className="transaction">
           <div className='left'>
-            <div className='name'>New Phone</div>
-            <div className='desc'>It was time for new phone</div>
+            <div className='name'>TV</div>
+            <div className='desc'>It was time for new TV</div>
           </div>
           <div className='right'>
-            <div className='price'>$200.00</div>
+            <div className='price red'>-$200.00</div>
             <div className='datetime'>12-02-2022 08:00</div>
           </div>
         </div>
         <div className="transaction">
           <div className='left'>
-            <div className='name'>New Phone</div>
+            <div className='name'>Salary</div>
             <div className='desc'>It was time for new phone</div>
           </div>
           <div className='right'>
-            <div className='price'>$200.00</div>
+            <div className='price green'>+$800.00</div>
             <div className='datetime'>12-02-2022 08:00</div>
           </div>
         </div>
         <div className="transaction">
           <div className='left'>
-            <div className='name'>New Phone</div>
+            <div className='name'>iPhone</div>
             <div className='desc'>It was time for new phone</div>
           </div>
           <div className='right'>
-            <div className='price'>$200.00</div>
+            <div className='price red'>-$900.00</div>
             <div className='datetime'>12-02-2022 08:00</div>
           </div>
         </div>
